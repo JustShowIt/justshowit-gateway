c597ccecafb521359bfc23ef32ddd9930c8f4749
Mario Linz <gelight@gmx.de>
2019-01-17 13:45:52 +0100

    Added comment with idea for deep learning

--- src/analyze.js
+++ src/analyze.js
@@ -6,27 +6,31 @@ const availableComponentTypes = ["text","video","textfield","link","image","arti
 // const neuronalNetwork = new brain.NeuralNetwork();
 const neuronalNetwork = new brain.recurrent.LSTM();
 
-/*
-NORMAL STRUCTURE
-const trainData = trainingData.map(data => {
-    let input = data.options;
-    let output = {};
-        output[data.type] = 1;
-    let trainData = { input: input, output: output };
-    return trainData;
-});
-console.log(trainData);
-*/ 
-
-/*
-LSTM STRUCTURE
-*/
 const networkTrainData = [];
+
+/* >>> Idee ...
+    https://www.youtube.com/watch?v=RVMHhtTqUxc >>> bei 16:45 min
+    ENCODE TEXT INPUT >>> "Dies ist ein test".split('').map(x => (x.charCodeAt(0) / 255));
+    ENCODED TEXT INPUT >>> [0.26666666666666666, 0.4117647058823529, 0.396078431372549, 0.45098039215686275, 0.12549019607843137, 0.4117647058823529, 0.45098039215686275, 0.4549019607843137, 0.12549019607843137, 0.396078431372549, 0.4117647058823529, 0.43137254901960786, 0.12549019607843137, 0.4549019607843137, 0.396078431372549, 0.45098039215686275, 0.4549019607843137]
+
+const trainData = [
+    {
+        input: "Dies ist ein test",
+        output: { test: 1 }
+    },
+    {
+        input: "Dies ist ein video",
+        output: { video: 1 }
+    }
+]
+*/
+
 trainingData.map(data => {
     let input = Object.keys(data.options).map(function(key, index) {
         networkTrainData.push({ input: key + " " + data.options[key], output: data.type });
     });
 });
+
 console.log("TRAINING DATA >>>", networkTrainData);
 
 neuronalNetwork.train(networkTrainData, {
