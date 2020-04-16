4bb964d6020d82a880d183f33aa906eefc0cba01
Mario Linz <gelight@gmx.de>
2020-04-16 21:26:10 +0200

    Adapted json structure of properties training data.

--- service/src/main/ts/gelight/justshowit/domain/analyze/properties-training-data.ts
+++ service/src/main/ts/gelight/justshowit/domain/analyze/properties-training-data.ts
@@ -1,11 +1,38 @@
 export default [
-    { input: ".mp4", output: "url" },
-    { input: "www.google.de", output: "url" },
-    { input: "www.", output: "url" },
-    { input: "http://www.", output: "url" },
-    { input: "https://www.", output: "url" },
-    { input: "abcdefghijklimopqrstuvwxyz", output: "text" },
-    { input: "01234564789", output: "text" },
-    { input: "Lorem ipsum dolor sit amet, consetetur sadipscing aliquyam erat, sed diam voluptua.", output: "text" },
-    { input: "Muh sagt die Kuh", output: "text" }
+    {
+        input: ".mp4",
+        output: "url"
+    },
+    {
+        input: "www.google.de",
+        output: "url"
+    },
+    {
+        input: "www.",
+        output: "url"
+    },
+    {
+        input: "http://www.",
+        output: "url"
+    },
+    {
+        input: "https://www.",
+        output: "url"
+    },
+    {
+        input: "abcdefghijklimopqrstuvwxyz",
+        output: "text"
+    },
+    {
+        input: "01234564789",
+        output: "text"
+    },
+    {
+        input: "Lorem ipsum dolor sit amet, consetetur sadipscing aliquyam erat, sed diam voluptua.",
+        output: "text"
+    },
+    {
+        input: "Muh sagt die Kuh",
+        output: "text"
+    }
 ];
